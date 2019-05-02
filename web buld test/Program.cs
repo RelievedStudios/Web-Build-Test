1a96f9aa143388901c2e419bad1cff3c0c581927
Owen Quinlan <46948241+RelievedStudios@users.noreply.github.com>
2019-05-02 19:43:18 +1000

    Init push

---
+++ web buld test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace web_buld_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string FreezeOpen = Console.ReadLine();
+        }
+    }
+}
